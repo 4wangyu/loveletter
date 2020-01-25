a66dfd295de9bd3f237fb0c27905c895a20605c2
4wangyu <4wangyu@gmail.com>
2020-01-26 03:56:18 +0800

    add encrypt form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,123 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Button, Radio, Input } from "zhui";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      encrypt: true,
+      prefixShow: false,
+      suffixShow: false,
+      prefix: "",
+      suffix: "",
+      secret: ""
+    };
+  }
+
+  toggleEncrypt() {
+    this.setState({ encrypt: !this.state.encrypt });
+  }
+
+  togglePrefixShow() {
+    this.setState({ prefixShow: !this.state.prefixShow });
+  }
+
+  toggleSuffixShow() {
+    this.setState({ suffixShow: !this.state.suffixShow });
+  }
+
+  setSecret(event) {
+    event.persist();
+    this.setState({ secret: event.target.value });
+  }
+
+  setPrefix(event) {
+    event.persist();
+    this.setState({ prefix: event.target.value });
+  }
+
+  setSuffix(event) {
+    event.persist();
+    this.setState({ suffix: event.target.value });
+  }
+
+  render() {
+    const {
+      encrypt,
+      prefixShow,
+      suffixShow,
+      prefix,
+      suffix,
+      secret
+    } = this.state;
+
+    return (
+      <div className="App">
+        <div className="toggle" onClick={() => this.toggleEncrypt()}>
+          <Radio
+            checked
+            text={encrypt ? "密encrypt" : "解decrypt"}
+            theme={encrypt ? "meihong" : "huaqing"}
+          />
+        </div>
+
+        {encrypt ? (
+          <div className="flex-col">
+            <Button.Group className="button-group">
+              <Button
+                round
+                theme={prefixShow ? "meihong" : "huaqing"}
+                onClick={() => this.togglePrefixShow()}
+              >
+                prefix
+              </Button>
+              <Button theme="meihong" className="secret-button">
+                secret
+              </Button>
+              <Button
+                round
+                theme={suffixShow ? "meihong" : "huaqing"}
+                onClick={() => this.toggleSuffixShow()}
+              >
+                suffix
+              </Button>
+            </Button.Group>
+
+            <div className="form flex-col">
+              {prefixShow && (
+                <Input
+                  width={`90%`}
+                  value={prefix}
+                  onChange={e => this.setPrefix(e)}
+                />
+              )}
+              <Input.Textarea
+                value={secret}
+                onChange={e => this.setSecret(e)}
+                width={`90%`}
+                className="secret"
+              />
+              {suffixShow && (
+                <Input
+                  width={`90%`}
+                  value={suffix}
+                  onChange={e => this.setSuffix(e)}
+                />
+              )}
+            </div>
+
+            <Button kong="meihong" className="encrypt-button">
+              Encrypt
+            </Button>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
